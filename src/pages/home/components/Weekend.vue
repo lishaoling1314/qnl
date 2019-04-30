8a7961d2c7c65ce6875947f896b1a69d015b12d6
L <940519229@qq.com>
2019-04-30 10:40:51 +0800

    project

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -4,10 +4,10 @@
         <ul>
             <li v-for="wk of weekList" :key="wk.id">
                 <img :src="wk.imgUrl">
-                <span>
-                    <h3>{{rd.desc}}</h3>
-                    <p>{{rd.text}}</p>
-                </span>
+                <div class="text">
+                    <h3>{{wk.desc}}</h3>
+                    <p>{{wk.text}}</p>
+                </div>
             </li>
         </ul>
     </div>
@@ -30,6 +30,18 @@ export default {
                 desc:'长隆野生动物世界',
                 text:'熊猫三胞胎与您共享欢乐时光熊猫三胞胎与您共享欢乐时光',
                 price:'269'
+            },{
+                id:'0003',
+                imgUrl:'https://lishaoling1314.github.io/qnl/src/assets/images/list5.jpg',
+                desc:'长隆野生动物世界',
+                text:'熊猫三胞胎与您共享欢乐时光熊猫三胞胎与您共享欢乐时光',
+                price:'269'
+            },{
+                id:'0004',
+                imgUrl:'https://lishaoling1314.github.io/qnl/src/assets/images/list6.jpg',
+                desc:'长隆野生动物世界',
+                text:'熊猫三胞胎与您共享欢乐时光熊猫三胞胎与您共享欢乐时光',
+                price:'269'
             }]
         }
     }
@@ -42,15 +54,19 @@ export default {
     padding:4% 2%
     font-size:0.35rem
 .weekend ul li
-    padding:2%
     background:#fff
     overflow:hidden
+.weekend li img 
+    width:100%
+.weekend li .text
+    padding:5% 2%
 .weekend li h3
     font-size:0.32rem
+    ellipsis()
 .weekend li p
-    line-height:0.4rem
-    padding:4% 0
+    line-height:0.6rem
     color:#666
+    ellipsis()
 </style>
 
 
