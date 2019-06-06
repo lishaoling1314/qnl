697d3881ce570b74cc30f1bcdb63ddb92b6f9774
L <940519229@qq.com>
2019-06-06 11:42:39 +0800

    porject

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <ul>
+            <li>A</li>
+            <li>A</li>
+            <li>A</li>
+            <li>A</li>
+            <li>A</li>
+            <li>A</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CityAlphabet',
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~style/varibles.styl'
+.list
+    display:flex
+    flex-direction:column
+    justify-content:center
+    position:absolute
+    right:0;
+    top:1.58rem
+    bottom:0
+    width:.4rem
+.list li
+    text-align:center
+    line-height:0.4rem
+</style>
+
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,42 +1,77 @@
 <template>
-    <div class="list">
-        <div class="area">
-            <h3>您的位置</h3>
-            <ul class="mp-list">
-                <li>北京</li>
-            </ul>
-        </div>
-        <div class="area">
-            <h3>热门城市</h3>
-            <ul class="mp-list">
-                <li>北京</li>
-                <li>西安</li>
-                <li>三亚</li>
-                <li>北京</li>
-                <li>西安</li>
-                <li>三亚</li>
-                <li>北京</li>
-                <li>西安</li>
-                <li>三亚</li>
-            </ul>
-        </div>
-        <div class="area">
-            <h3>A</h3>
-            <ul class="local-list">
-                <li>北京</li>
-            </ul>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <h3>您的位置</h3>
+                <ul class="mp-list">
+                    <li>北京</li>
+                </ul>
+            </div>
+            <div class="area">
+                <h3>热门城市</h3>
+                <ul class="mp-list">
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                </ul>
+            </div>
+            <div class="area">
+                <h3>A</h3>
+                <ul class="local-list">
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                </ul>
+            </div>
+            <div class="area">
+                <h3>B</h3>
+                <ul class="local-list">
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                    <li>北京</li>
+                    <li>西安</li>
+                    <li>三亚</li>
+                </ul>
+            </div>      
         </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name:'CityList'
+    name:'CityList',
+    mounted(){
+        this.scroll=new Bscroll(this.$refs.wrapper)
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~style/varibles.styl'
+.list
+    overflow:hidden
+    position:absolute
+    top:1.5rem
+    left:0
+    right:0
+    bottom:0
 .area h3
     line-height:.6rem
     padding:0 .2rem
@@ -58,6 +93,8 @@ export default {
 .local-list li
     line-height:.6rem
     padding-left:2%
-    border-bottom:1px solid #ccc
+    border-bottom:1px solid #f6f3f3
+.local-list li:nth-last-child(1)
+    border-bottom:none
 </style>
 
